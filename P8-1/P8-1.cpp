072df599d362ad112666969c466add008900748c
Sun-biscuit <g-fuller@onu.edu>
2024-10-30 17:32:39 -0400

    Completed countCharacter function

--- P8-1/P8-1.cpp
+++ P8-1/P8-1.cpp
@@ -1,1 +1,18 @@
-      
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int countCharacter(string);
+
+int main() {
+
+	string goblygook = "Zoinks Scoob";
+	cout << countCharacter(goblygook);
+
+
+}
+
+int countCharacter(string str) {
+	return str.length();
+}
